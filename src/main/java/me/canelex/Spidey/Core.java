b6b0de5e70c5284f712992604573e1eab6a0126f
Sebo Molnár <canelex@ymastersk.net>
2018-12-27 23:35:24 +0100

    Create Core.java

---
+++ src/main/java/me/canelex/Spidey/Core.java
@@ -0,0 +1,21 @@
+package me.canelex.Spidey;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.Game;
+
+public class Core {	
+							
+	public static void main( String[] args ) throws Exception {
+    	
+    	@SuppressWarnings("unused")
+		JDA jda = new JDABuilder(AccountType.BOT)
+    			.setToken(Secrets.token)
+    			.addEventListener(new Eventy())
+    			.setGame(Game.streaming("s!help", "https://twitch.tv/canelex_"))    			
+    			.build().awaitReady();               
+        
+    }                        
+    
+}
