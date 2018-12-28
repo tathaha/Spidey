ddb0dc99d5954d646449cc0413d72c7f0082e7cd
Sebo Molnár <canelex@ymastersk.net>
2018-12-28 16:07:30 +0100

    Create Secrets.java

---
+++ src/main/java/me/canelex/Spidey/Secrets.java
@@ -0,0 +1,12 @@
+package me.canelex.Spidey;
+
+public class Secrets {
+	
+	public static String token = "";
+	public static String host = "localhost";
+	public static String database = "";
+	public static String username = "";
+	public static String port = "3306";	
+	public static String pass = "";
+
+}
