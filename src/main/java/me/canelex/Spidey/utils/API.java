3ece145df54228cb59fb33d5d53e1a7d32ab4eed
caneleex <canelex@ymastersk.net>
2019-02-02 16:50:39 +0100

    New API method

--- src/main/java/me/canelex/Spidey/utils/API.java
+++ src/main/java/me/canelex/Spidey/utils/API.java
@@ -121,6 +121,12 @@ public class API {
 		
 		return g.getFeatures().contains("VIP_REGIONS"); 	
 		
-	}	
+	}
+	
+    public static String replaceLast(final String text, final String regex, final String replacement) {
+    	
+        return text.replaceFirst("(?s)(.*)" + regex, "$1" + replacement);
+        
+    }  	
 
 }
