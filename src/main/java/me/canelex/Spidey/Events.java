d861b826a6ffab1686b5f22b903374b4fa844d66
caneleex <canelex@ymastersk.net>
2019-02-02 16:51:22 +0100

    Uptime command

--- src/main/java/me/canelex/Spidey/Events.java
+++ src/main/java/me/canelex/Spidey/Events.java
@@ -1,6 +1,7 @@
 package me.canelex.Spidey;
 
 import java.awt.Color;
+import java.lang.management.ManagementFactory;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -538,6 +539,26 @@ public class Events extends ListenerAdapter {
     		
     		API.deleteMessage(msg);
     		API.sendPrivateMessage(e.getAuthor(), "**Soon**");
+    		
+    	} 
+    	
+    	if (msg.getContentRaw().equalsIgnoreCase("s!uptime")) {
+    		
+    		final long duration = ManagementFactory.getRuntimeMXBean().getUptime();
+
+            final long years = duration / 31104000000L;
+            final long months = duration / 2592000000L % 12;
+            final long days = duration / 86400000L % 30;
+            final long hours = duration / 3600000L % 24;
+            final long minutes = duration / 60000L % 60;
+            final long seconds = duration / 1000L % 60;
+
+            String uptime = (years == 0 ? "" : "**" + years + "**r, ") + (months == 0 ? "" : "**" + months + "**mo, ") + (days == 0 ? "" : "**" + days + "**d, ") + (hours == 0 ? "" : "**" + hours + "**h, ") + (minutes == 0 ? "" : "**" + minutes + "**m, ") + (seconds == 0 ? "" : "**" + seconds + "**s, ");
+
+            uptime = API.replaceLast(uptime, ", ", "");
+            uptime = API.replaceLast(uptime, ",", " a");
+
+            API.sendMessage(msgCh, "Uptime: " + uptime + "");    		
     		
     	}    	
         
