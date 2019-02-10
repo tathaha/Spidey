b5e1ae8f27e3dd6f02da4f3f0c2b01ab44740aa5
caneleex <canelex@ymastersk.net>
2019-02-10 20:22:38 +0100

    Made jda accessible from other classes

--- src/main/java/me/canelex/Spidey/Core.java
+++ src/main/java/me/canelex/Spidey/Core.java
@@ -6,11 +6,12 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 
 public class Core {	
+	
+	public static JDA jda;
 							
 	public static void main( String[] args ) throws Exception {
     	
-    	@SuppressWarnings("unused")
-		JDA jda = new JDABuilder(AccountType.BOT)
+		jda = new JDABuilder(AccountType.BOT)
     			.setToken(Secrets.token)
     			.addEventListeners(new Events())
     			.setActivity(Activity.streaming("s!help", "https://twitch.tv/canelex_"))    			
