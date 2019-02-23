2aef6fd15daad28e265d7c995343b6d24d925e1e
caneleex <canelex@ymastersk.net>
2019-02-23 18:09:01 +0100

    Made emojis final

--- src/main/java/me/canelex/Spidey/utils/Emoji.java
+++ src/main/java/me/canelex/Spidey/utils/Emoji.java
@@ -2,8 +2,8 @@ package me.canelex.Spidey.utils;
 
 public interface Emoji {
 	
-	String like = "\uD83D\uDC4D";
-	String dislike = "\uD83D\uDC4E";
-	String shrug = "\uD83E\uDD37";	
+	final String like = "\uD83D\uDC4D";
+	final String dislike = "\uD83D\uDC4E";
+	final String shrug = "\uD83E\uDD37";	
 
 }
