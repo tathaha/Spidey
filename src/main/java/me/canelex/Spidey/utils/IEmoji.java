8a72b194a13eaa780269ebc672bb47c8fe6d5089
caneleex <canelex@ymastersk.net>
2019-03-24 22:30:16 +0100

    Use of class for constants

--- src/main/java/me/canelex/Spidey/utils/IEmoji.java
+++
@@ -1,9 +0,0 @@
-package me.canelex.Spidey.utils;
-
-public interface IEmoji {
-	
-	final String like = "\uD83D\uDC4D";
-	final String dislike = "\uD83D\uDC4E";
-	final String shrug = "\uD83E\uDD37";	
-
-}
