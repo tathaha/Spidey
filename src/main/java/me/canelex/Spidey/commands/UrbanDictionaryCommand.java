74aaa863a4a738cb222fb891e90229ee950cc1cc
caneleex <canelex@ymastersk.net>
2019-04-22 20:31:52 +0200

    Fixed duplicate error message

--- src/main/java/me/canelex/Spidey/commands/UrbanDictionaryCommand.java
+++ src/main/java/me/canelex/Spidey/commands/UrbanDictionaryCommand.java
@@ -22,59 +22,52 @@ public class UrbanDictionaryCommand implements ICommand {
 	public final boolean called(final GuildMessageReceivedEvent e) {
 
 		return true;
-		
+
 	}
 
 	@Override
-	public final void action(final GuildMessageReceivedEvent e) {	
-		
+	public final void action(final GuildMessageReceivedEvent e) {
+
 		final String query = e.getMessage().getContentRaw().substring(5);
-		
-	    try {
-	    	
-	    	final Future<HttpResponse<JsonNode>> future = Unirest.get("http://api.urbandictionary.com/v0/define?term=" + URLEncoder.encode(query, "UTF-8")).asJsonAsync();
-	    	final HttpResponse<JsonNode> json = future.get(30, TimeUnit.SECONDS);
-	    	final JSONArray list = json.getBody().getObject().getJSONArray("list");
-            
-            if (list.length() == 0) {
-            	
-    	    	API.sendMessage(e.getChannel(), ":no_entry: Query not found", false);            	
-            	
-            }
-            
-            final JSONObject item = list.getJSONObject(0);
-            final String result = String.format("Urban Dictionary \n\n"
-                            + "Definition for **%s**: \n"
-                            + "```\n"
-                            + "%s\n"
-                            + "```\n"
-                            + "**example**: \n"
-                            + "%s" + "\n\n"
-                            + "_by %s (" + Emojis.like + "%s  " + Emojis.dislike + "%s)_"
-                    , item.getString("word"), item.getString("definition"), item.getString("example"),
-                    item.getString("author"), item.getInt("thumbs_up"), item.getInt("thumbs_down"));
-            
-            API.sendMessage(e.getChannel(), result, false);
-	    	
-	    }		
-	    
-	    catch (final Exception ex) {
-	    	
-	    	ex.printStackTrace();
-	    	API.sendMessage(e.getChannel(), ":no_entry: A problem has occured: **" + ex.getMessage() + "**", false);
-	    	
-	    }
-		
+
+		try {
+
+			final Future<HttpResponse<JsonNode>> future = Unirest.get("http://api.urbandictionary.com/v0/define?term=" + URLEncoder.encode(query, "UTF-8")).asJsonAsync();
+			final HttpResponse<JsonNode> json = future.get(30, TimeUnit.SECONDS);
+			final JSONArray list = json.getBody().getObject().getJSONArray("list");
+
+			final JSONObject item = list.getJSONObject(0);
+			final String result = String.format("Urban Dictionary \n\n"
+							+ "Definition for **%s**: \n"
+							+ "```\n"
+							+ "%s\n"
+							+ "```\n"
+							+ "**example**: \n"
+							+ "%s" + "\n\n"
+							+ "_by %s (" + Emojis.like + "%s  " + Emojis.dislike + "%s)_"
+					, item.getString("word"), item.getString("definition"), item.getString("example"),
+					item.getString("author"), item.getInt("thumbs_up"), item.getInt("thumbs_down"));
+
+			API.sendMessage(e.getChannel(), result, false);
+
+		}
+
+		catch (final Exception ex) {
+
+			API.sendMessage(e.getChannel(), ":no_entry: Query not found.", false);
+
+		}
+
 	}
 
 	@Override
 	public final String help() {
 
 		return "Returns a definition of your query from Urban Dictionary";
-		
+
 	}
 
 	@Override
 	public final void executed(final boolean success, final GuildMessageReceivedEvent e) {}
-	
+
 }
