8ce6e841bf1172bfde18bcd96ca5addcfd5c389a
caneleex <git@mlnr.dev>
2020-03-10 21:28:20 +0100

    Possibly fix an issue

--- src/main/java/me/canelex/spidey/commands/MembercountCommand.java
+++ src/main/java/me/canelex/spidey/commands/MembercountCommand.java
@@ -12,36 +12,35 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused", "ConstantConditions"})
 public class MembercountCommand implements ICommand
 {
 	@Override
 	public final void action(final String[] args, final Message message)
 	{
-		final var mcv = message.getGuild().getMemberCache();
-		final var tonline = mcv.stream().filter(member -> member.getOnlineStatus() == OnlineStatus.ONLINE || member.getOnlineStatus() == OnlineStatus.IDLE || member.getOnlineStatus() == OnlineStatus.DO_NOT_DISTURB).collect(Collectors.toList());
-		final var bonline = tonline.stream().filter(m -> m.getUser().isBot()).count();
-		final var total = mcv.size();
-		final var online = tonline.size();
-		final var bots = mcv.stream().filter(member -> member.getUser().isBot()).count();
-		final var ponline = online - bonline;
-		final var monline = mcv.stream().filter(Utils::isMobile).count();
-		final var wonline = mcv.stream().filter(Utils::isWeb).count() - bonline;
-		final var donline = mcv.stream().filter(Utils::isDesktop).count();
+		final var memberCache = message.getGuild().getMemberCache();
+		final var bots = memberCache.applyStream(stream -> stream.filter(member -> member.getUser().isBot()));
+		final var totalOnline = memberCache.applyStream(stream -> stream.filter(member -> member.getOnlineStatus() == OnlineStatus.DO_NOT_DISTURB || member.getOnlineStatus() == OnlineStatus.IDLE || member.getOnlineStatus() == OnlineStatus.ONLINE).collect(Collectors.toList()));
+		final var botsOnline = totalOnline.stream().filter(member -> member.getUser().isBot()).count();
+		final var humansOnline = totalOnline.size() - botsOnline;
+		final var desktopOnline = memberCache.applyStream(stream -> stream.filter(Utils::isDesktop)).count();
+		final var mobileOnline = memberCache.applyStream(stream -> stream.filter(Utils::isMobile)).count();
+		final var webOnline = memberCache.applyStream(stream -> stream.filter(Utils::isWeb)).count();
+		final var total = memberCache.size();
 
 		final var eb = Utils.createEmbedBuilder(message.getAuthor());
 		eb.setAuthor("MEMBERCOUNT");
 		eb.setColor(Color.WHITE);
 		eb.setTimestamp(Instant.now());
 		eb.addField("Total", "**" + total + "**", true);
-		eb.addField("People", "**" + (total - bots) + "**", true);
+		eb.addField("People", "**" + (total - bots.count()) + "**", true);
 		eb.addField("Bots", "**" + bots + "**", true);
-		eb.addField("Total online", "**" + online + "**", true);
-		eb.addField("People online", "**" + ponline + "**", true);
-		eb.addField("Bots online", "**" + bonline + "**", true);
-		eb.addField("Desktop users online", "**" + donline + "**", true);
-		eb.addField("Mobile users online", "**" + monline + "**", true);
-		eb.addField("Web users online", "**" + wonline + "**", true);
+		eb.addField("Total online", "**" + totalOnline + "**", true);
+		eb.addField("Humans online", "**" + humansOnline + "**", true);
+		eb.addField("Bots online", "**" + botsOnline + "**", true);
+		eb.addField("Desktop users online", "**" + desktopOnline + "**", true);
+		eb.addField("Mobile users online", "**" + mobileOnline + "**", true);
+		eb.addField("Web users online", "**" + webOnline + "**", true);
 		Utils.sendMessage(message.getChannel(), eb.build());
 	}
 
