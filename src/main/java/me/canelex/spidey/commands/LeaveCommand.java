a4517530f6245ae26d04430220a05bd889dd2652
caneleex <git@mlnr.dev>
2020-03-27 15:56:29 +0100

    Fix / add required permission to leave command

--- src/main/java/me/canelex/spidey/commands/LeaveCommand.java
+++ src/main/java/me/canelex/spidey/commands/LeaveCommand.java
@@ -1,9 +1,10 @@
 package me.canelex.spidey.commands;
 
+import me.canelex.jda.api.Permission;
 import me.canelex.jda.api.entities.Message;
-import me.canelex.spidey.MySQL;
 import me.canelex.spidey.objects.command.Category;
 import me.canelex.spidey.objects.command.ICommand;
+import me.canelex.spidey.utils.PermissionError;
 import me.canelex.spidey.utils.Utils;
 
 import java.util.concurrent.TimeUnit;
@@ -15,23 +16,23 @@ public class LeaveCommand implements ICommand
 	public final void action(final String[] args, final Message message)
 	{
 		final var guild = message.getGuild();
-		final var idLong = guild.getIdLong();
 		final var channel = message.getChannel();
-		
-		if (!message.getMember().isOwner())
-			Utils.sendMessage(channel, message.getAuthor().getAsMention() + ", you have to be the guild owner to do this.");
+
+		final var requiredPermission = getRequiredPermission();
+		if (Utils.hasPerm(message.getMember(), requiredPermission))
+			Utils.sendMessage(channel, PermissionError.getErrorMessage(requiredPermission));
 		else
 		{
 			channel.sendMessage("Bye.").queue(m -> m.delete().queueAfter(5, TimeUnit.SECONDS));
 			Utils.deleteMessage(message);
-			Utils.sendPrivateMessageFormat(guild.getOwner().getUser(), "I've left your server **%s**. If you'd want to invite me back, please use this URL: ||%s||. Thanks for using **Spidey**!", false, guild.getName(), Utils.getInviteUrl(idLong));
-			MySQL.removeChannel(idLong);
 			guild.leave().queue();
 		}
 	}
 
 	@Override
 	public final String getDescription() { return "Spidey will leave your server"; }
+	@Override
+	public final Permission getRequiredPermission() { return Permission.ADMINISTRATOR; }
 	@Override
 	public final String getInvoke() { return "leave"; }
 	@Override
