ba099268e7a588887c069cc29084d956448f70cf
caneleex <git@mlnr.dev>
2020-05-11 19:19:18 +0200

    First pass on preventing from deleting pinned messages

--- src/main/java/me/canelex/spidey/commands/moderation/DeleteCommand.java
+++ src/main/java/me/canelex/spidey/commands/moderation/DeleteCommand.java
@@ -2,12 +2,17 @@ package me.canelex.spidey.commands.moderation;
 
 import me.canelex.jda.api.Permission;
 import me.canelex.jda.api.entities.Message;
+import me.canelex.jda.api.events.message.guild.react.GuildMessageReactionAddEvent;
+import me.canelex.spidey.Core;
 import me.canelex.spidey.objects.command.Category;
 import me.canelex.spidey.objects.command.ICommand;
+import me.canelex.spidey.utils.Emojis;
 import me.canelex.spidey.utils.Utils;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @SuppressWarnings("unused")
@@ -52,17 +57,52 @@ public class DeleteCommand implements ICommand
 				Utils.returnError("Please enter a number from 1-100", message);
 				return;
 			}
-			if (amount == 100)
-				amount = 99;
 
-			channel.getIterableHistory().cache(false).takeAsync(amount).thenAccept(messages ->
+			channel.getIterableHistory().cache(false).takeAsync(amount).thenAcceptAsync(messages ->
 			{
-				count = messages.size();
+				final var toDelete = new ArrayList<>(messages);
+				final var pinned = messages.stream().filter(Message::isPinned).count();
+				if (pinned > 0)
+				{
+					final var equalsOne = pinned == 1;
+					channel.sendMessage("There " + (equalsOne ? "is" : "are") + " **" + pinned + "** pinned message" + (equalsOne ? "" : "s") + " that you're going to delete. Are you sure?").queue(msg ->
+					{
+						final var wastebasket = "\uD83D\uDDD1";
+						msg.addReaction(Emojis.CHECK).queue();
+						msg.addReaction(Emojis.CROSS).queue();
+						msg.addReaction(wastebasket).queue();
+						Core.getWaiter().waitForEvent(GuildMessageReactionAddEvent.class,
+								ev ->
+								{
+									final var name = ev.getReactionEmote().getName();
+									return ev.getUser() == message.getAuthor() && ev.getMessageIdLong() == msg.getIdLong() && (name.equals(Emojis.CHECK) || name.equals(Emojis.CROSS) || name.equals(wastebasket));
+								},
+								ev ->
+								{
+									switch (ev.getReactionEmote().getName())
+									{
+										case Emojis.CHECK:
+											Utils.deleteMessage(msg);
+											break;
+										case Emojis.CROSS:
+											Utils.deleteMessage(msg);
+											return;
+										case wastebasket:
+											toDelete.removeIf(Message::isPinned);
+											final var size = messages.size();
+											channel.getHistoryAfter(messages.get(size - 1), size - Math.toIntExact(pinned)).queue(msgs -> toDelete.addAll(msgs.getRetrievedHistory()));
+											Utils.deleteMessage(msg);
+											break;
+									}
+								}, 1, TimeUnit.MINUTES, () -> Utils.returnError("Sorry, but you took too long", msg));
+					});
+				}
+				count = toDelete.size();
 				CompletableFuture<Void> future;
 				if (count == 1)
-					future = messages.get(0).delete().submit();
+					future = toDelete.get(0).delete().submit();
 				else
-					future = CompletableFuture.allOf(channel.purgeMessages(messages).toArray(new CompletableFuture[0]));
+					future = CompletableFuture.allOf(channel.purgeMessages(toDelete).toArray(new CompletableFuture[0]));
 				future.thenRunAsync(() -> channel.sendMessage(Utils.generateSuccess(count, null))
 						                         .delay(Duration.ofSeconds(5))
 					   							 .flatMap(Message::delete)
