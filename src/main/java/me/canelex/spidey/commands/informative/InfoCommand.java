700338138b922c3b2a6c382f345936f053cb8618
caneleex <git@mlnr.dev>
2020-05-18 19:14:08 +0200

    Fix paypal link

--- src/main/java/me/canelex/spidey/commands/informative/InfoCommand.java
+++ src/main/java/me/canelex/spidey/commands/informative/InfoCommand.java
@@ -43,7 +43,7 @@ public class InfoCommand extends Command
 			jda.getRestPing().queue(ping ->
 			{
 				final var eb = Utils.createEmbedBuilder(author);
-				eb.setAuthor("About me", "https://paypal.me/canelex", jda.getSelfUser().getAvatarUrl());
+				eb.setAuthor("About me", "https://paypal.me/caneleex", jda.getSelfUser().getAvatarUrl());
 				eb.setColor(Color.WHITE);
 				eb.addField("Developer", info.getOwner().getAsMention(), false);
 				eb.addField("Ping", "**" + jda.getGatewayPing() + "**, **" + ping + "** ms", false);
