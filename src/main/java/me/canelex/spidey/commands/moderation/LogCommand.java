fff8a396ece1c43d75d5db45316014ba889dcb4e
caneleex <git@mlnr.dev>
2020-05-28 16:18:51 +0200

    Change log command behavior

--- src/main/java/me/canelex/spidey/commands/moderation/LogCommand.java
+++ src/main/java/me/canelex/spidey/commands/moderation/LogCommand.java
@@ -7,8 +7,6 @@ import me.canelex.spidey.utils.Utils;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Message;
 
-import java.time.Duration;
-
 @SuppressWarnings("unused")
 public class LogCommand extends Command
 {
@@ -21,41 +19,29 @@ public class LogCommand extends Command
 	public final void execute(final String[] args, final Message message)
 	{
 		final var guild = message.getGuild();
-		final var idLong = guild.getIdLong();
+		final var guildId = guild.getIdLong();
 		final var channel = message.getChannel();
-
 		final var requiredPermission = getRequiredPermission();
-		if (Utils.hasPerm(message.getMember(), requiredPermission))
-		{
-			Utils.deleteMessage(message);
-			if (guild.getSystemChannel() != null)
-				guild.getManager().setSystemChannel(null).queue();
 
-			final var channelId = channel.getIdLong();
-			if (MySQL.getChannel(idLong) == channelId)
-			{
-				final var defaultChannel = guild.getDefaultChannel();
-				if (defaultChannel == null)
-					Utils.returnError("There is no default channel to set as the log channel", message);
-				else
-				{
-					MySQL.setChannel(idLong, defaultChannel.getIdLong());
-					channel.sendMessage(":white_check_mark: The log channel has been set to " + defaultChannel.getAsMention() + ". Type this command again in the channel you want to set as the log channel.")
-						   .delay(Duration.ofSeconds(5))
-						   .flatMap(Message::delete)
-						   .queue();
-				}
-			}
-			else
-			{
-				MySQL.setChannel(idLong, channelId);
-				channel.sendMessage(":white_check_mark: The log channel has been set to <#" + channelId + ">. Type this command again to set the log channel to the default guild channel (if present).")
-					   .delay(Duration.ofSeconds(5))
-					   .flatMap(Message::delete)
-					   .queue();
-			}
+		if (!Utils.hasPerm(message.getMember(), requiredPermission))
+		{
+			Utils.getPermissionsError(requiredPermission, message);
+			return;
+		}
+
+		if (guild.getSystemChannel() != null)
+			guild.getManager().setSystemChannel(null).queue();
+
+		final var channelId = channel.getIdLong();
+		if (MySQL.getChannel(guildId) == channelId)
+		{
+			MySQL.setChannel(guildId, 0);
+			Utils.sendMessage(channel, ":white_check_mark: The log channel has been reset!");
 		}
 		else
-			Utils.getPermissionsError(requiredPermission, message);
+		{
+			MySQL.setChannel(guildId, channelId);
+			Utils.sendMessage(channel, ":white_check_mark: The log channel has been set to <#" + channelId + ">!");
+		}
 	}
 }
