42225b20eee5ed83f80e2dd843113c66a09de33f
caneleex <git@mlnr.dev>
2020-06-14 22:13:37 +0200

    no

--- src/main/java/me/canelex/spidey/Core.java
+++ src/main/java/me/canelex/spidey/Core.java
@@ -27,7 +27,7 @@ public class Core
 	{
 		try
 		{
-			JDABuilder.create(System.getenv("SpideyDev"), EnumSet.of(GUILD_BANS, GUILD_INVITES, GUILD_MEMBERS, GUILD_MESSAGES, GUILD_MESSAGE_REACTIONS, GUILD_EMOJIS))
+			JDABuilder.create(System.getenv("Spidey"), EnumSet.of(GUILD_BANS, GUILD_INVITES, GUILD_MEMBERS, GUILD_MESSAGES, GUILD_MESSAGE_REACTIONS, GUILD_EMOJIS))
 					.disableCache(CacheFlag.MEMBER_OVERRIDES)
 					.addEventListeners(new Events(), waiter)
 					.setStatus(OnlineStatus.DO_NOT_DISTURB)
