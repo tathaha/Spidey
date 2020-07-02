8343b1fc6d4d342e3601f8fa271875013d3754f6
caneleex <git@mlnr.dev>
2020-07-02 15:33:08 +0200

    Generate a color based on random value

--- src/main/java/dev/mlnr/spidey/commands/fun/HowGay.java
+++ src/main/java/dev/mlnr/spidey/commands/fun/HowGay.java
@@ -6,7 +6,6 @@ import dev.mlnr.spidey.utils.Utils;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Message;
 
-import java.awt.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 @SuppressWarnings("unused")
@@ -24,7 +23,8 @@ public class HowGay extends Command
         final var random = ThreadLocalRandom.current().nextInt(0, 100 + 1); // values from 0 to 100, 100 + 1 'cause 100 has to be inclusive
         final var eb = Utils.createEmbedBuilder(msg.getAuthor());
         eb.setAuthor("gay rate");
-        eb.setColor(Color.MAGENTA);
+        eb.setColor(getColorHex(random));
+
         if (args.length == 0)
             eb.setDescription("you are **" + random + "**% gay " + prideFlag);
         else if (args.length == 1)
@@ -43,5 +43,12 @@ public class HowGay extends Command
             return;
         }
         Utils.sendMessage(msg.getTextChannel(), eb.build());
+    }
+
+    private int getColorHex(final int value)
+    {
+        final var r = ((255 * value) / 100);
+        final var g = (255 * (100 - value)) / 100;
+        return ((r & 0x0ff) << 16) | ((g & 0x0ff) << 8) | (0);
     }
 }
