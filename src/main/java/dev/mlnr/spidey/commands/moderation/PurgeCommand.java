e7a378192d5569277a2e20db196d767faae8b23f
caneleex <git@mlnr.dev>
2020-08-02 18:48:12 +0200

    Optimize purge command

--- src/main/java/dev/mlnr/spidey/commands/moderation/PurgeCommand.java
+++ src/main/java/dev/mlnr/spidey/commands/moderation/PurgeCommand.java
@@ -17,7 +17,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-import static dev.mlnr.spidey.utils.Utils.addReaction;
+import static dev.mlnr.spidey.utils.Utils.*;
 
 @SuppressWarnings({"unused", "StringBufferReplaceableByString"})
 public class PurgeCommand extends Command
@@ -35,13 +35,13 @@ public class PurgeCommand extends Command
 
         if (!channel.getGuild().getSelfMember().hasPermission(channel, getRequiredPermission(), Permission.MESSAGE_HISTORY))
         {
-            Utils.returnError("I don't have permission to purge messages or see the message history in this channel", message);
+            returnError("I don't have permission to purge messages or see the message history in this channel", message);
             return;
         }
 
         if (args.length == 0)
         {
-            Utils.returnError("Wrong syntax", message);
+            returnError("Wrong syntax", message);
             return;
         }
 
@@ -50,14 +50,14 @@ public class PurgeCommand extends Command
         {
             amount = Integer.parseInt(args[0]);
         }
-        catch (final NumberFormatException ignored)
+        catch (final NumberFormatException ex)
         {
-            Utils.returnError("Entered value is either negative or not a number", message);
+            returnError("Entered value is either negative or not a number", message);
             return;
         }
         if (amount > 100 || amount < 1)
         {
-            Utils.returnError("Please enter a number from 1-100", message);
+            returnError("Please enter a number from 1-100", message);
             return;
         }
 
@@ -65,12 +65,17 @@ public class PurgeCommand extends Command
         final var user = mentioned.isEmpty() ? null : mentioned.get(0);
         final var limit = amount;
 
-        message.delete().queue(ignored -> channel.getIterableHistory().cache(false).takeAsync(100).thenAcceptAsync(messages ->
+        message.delete().queue(ignored -> channel.getIterableHistory().cache(false).limit(user == null ? limit : 100).queue(messages ->
         {
-            final var msgs = user == null ? messages.subList(0, Math.min(messages.size(), limit)) : messages.stream().filter(msg -> msg.getAuthor().equals(user)).limit(limit).collect(Collectors.toList());
+            if (messages.isEmpty())
+            {
+                returnError("There are no messages to be deleted", message);
+                return;
+            }
+            final var msgs = user == null ? messages : messages.stream().filter(msg -> msg.getAuthor().equals(user)).limit(limit).collect(Collectors.toList());
             if (msgs.isEmpty())
             {
-                Utils.returnError("There are no messages to be deleted", message);
+                returnError("There are no messages by user **" + user.getAsTag() + "** to be deleted", message);
                 return;
             }
             final var pinned = msgs.stream().filter(Message::isPinned).collect(Collectors.toList());
@@ -80,13 +85,12 @@ public class PurgeCommand extends Command
                 return;
             }
             final var size = pinned.size();
-            final var equalsOne = size == 1;
             final var builder = new StringBuilder("There ");
-            builder.append(equalsOne ? "is" : "are").append(" **").append(size)
-                    .append("** pinned message").append(equalsOne ? "" : "s").append(" selected for deletion. ")
-                    .append("Are you sure you want to delete ").append(equalsOne ? "it" : "them").append("? ")
+            builder.append(size == 1 ? "is" : "are").append(" **").append(size)
+                    .append("** pinned message").append(size == 1 ? "" : "s").append(" selected for deletion. ")
+                    .append("Are you sure you want to delete ").append(size == 1 ? "it" : "them").append("? ")
                     .append("Deleting a message will also unpin it.")
-                    .append("\n\nReacting with :white_check_mark: will delete ").append(equalsOne ? "this message" : "these messages").append(".")
+                    .append("\n\nReacting with :white_check_mark: will delete ").append(size == 1 ? "this message" : "these messages").append(".")
                     .append("\nReacting with :wastebasket: will delete each unpinned message.")
                     .append("\nReacting with :x: will cancel the deletion.")
                     .append("\n\nThe deletion will be cancelled automatically in **1 minute** if a decision isn't made.");
@@ -98,44 +102,40 @@ public class PurgeCommand extends Command
                 addReaction(msg, Emojis.CROSS);
 
                 Core.getWaiter().waitForEvent(GuildMessageReactionAddEvent.class,
-                    ev -> ev.getUser() == message.getAuthor() && ev.getMessageIdLong() == msg.getIdLong(),
-                    ev ->
-                    {
-                        switch (ev.getReactionEmote().getName())
+                        ev -> ev.getUser() == message.getAuthor() && ev.getMessageIdLong() == msg.getIdLong(),
+                        ev ->
                         {
-                            case Emojis.CHECK:
-                                Utils.deleteMessage(msg);
-                                break;
-                            case Emojis.CROSS:
-                                Utils.deleteMessage(msg);
-                                return;
-                            case wastebasket:
-                                msgs.removeAll(pinned);
-                                if (msgs.isEmpty())
-                                {
-                                    Utils.returnError("There are no messages to be deleted", msg);
+                            switch (ev.getReactionEmote().getName())
+                            {
+                                case Emojis.CHECK:
+                                    deleteMessage(msg);
+                                    break;
+                                case Emojis.CROSS:
+                                    deleteMessage(msg);
                                     return;
-                                }
-                                Utils.deleteMessage(msg);
-                                break;
-                            default:
-                        }
-                        proceed(msgs, user, channel);
-                    }, 1, TimeUnit.MINUTES, () -> Utils.returnError("Sorry, you took too long", msg));
-                });
-        }).exceptionally(throwable ->
-        {
-            Utils.returnError("Unfortunately, i couldn't purge messages due to an internal error: **" + throwable.getMessage() + "**. Please report this message to the Developer", message);
-            return null;
-        }));
+                                case wastebasket:
+                                    msgs.removeAll(pinned);
+                                    deleteMessage(msg);
+                                    if (msgs.isEmpty())
+                                    {
+                                        returnError("There are no unpinned messages to be deleted", msg);
+                                        return;
+                                    }
+                                    break;
+                                default:
+                            }
+                            proceed(msgs, user, channel);
+                        }, 1, TimeUnit.MINUTES, () -> returnError("Sorry, you took too long", msg));
+            });
+        }, throwable -> returnError("Unfortunately, i couldn't purge messages due to an internal error: **" + throwable.getMessage() + "**. Please report this message to the Developer", message)));
     }
 
     private void proceed(final List<Message> toDelete, final User user, final TextChannel channel)
     {
         final var future = CompletableFuture.allOf(channel.purgeMessages(toDelete).toArray(new CompletableFuture[0]));
         future.thenRunAsync(() -> channel.sendMessage(Utils.generateSuccess(toDelete.size(), user))
-                                         .delay(Duration.ofSeconds(5))
-                                         .flatMap(Message::delete)
-                                         .queue());
+                .delay(Duration.ofSeconds(5))
+                .flatMap(Message::delete)
+                .queue());
     }
 }
