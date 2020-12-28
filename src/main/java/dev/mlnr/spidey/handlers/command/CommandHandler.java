16c11056f6943092682c5297f21bb9682a2a6b70
caneleex <git@mlnr.dev>
2020-12-28 19:58:05 +0100

    fix formatting

--- src/main/java/dev/mlnr/spidey/handlers/command/CommandHandler.java
+++ src/main/java/dev/mlnr/spidey/handlers/command/CommandHandler.java
@@ -20,87 +20,87 @@ import static dev.mlnr.spidey.handlers.command.CooldownHandler.isOnCooldown;
 
 public class CommandHandler
 {
-	private static final Map<String, Command> COMMANDS = new HashMap<>();
-	private static final Logger LOGGER = LoggerFactory.getLogger(CommandHandler.class);
-	private static final String NO_PERMS = "Action can't be completed because you don't have the **%s** permission";
+    private static final Map<String, Command> COMMANDS = new HashMap<>();
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommandHandler.class);
+    private static final String NO_PERMS = "Action can't be completed because you don't have the **%s** permission";
 
-	private CommandHandler() {}
+    private CommandHandler() {}
 
-	static
-	{
-		try (final var result = new ClassGraph().acceptPackages("dev.mlnr.spidey.commands").scan())
-		{
-			for (final var cls : result.getAllClasses())
-			{
-				final var cmd = (Command) cls.loadClass().getDeclaredConstructor().newInstance();
-				COMMANDS.put(cmd.getInvoke(), cmd);
-				for (final var alias : cmd.getAliases())
-					COMMANDS.put(alias, cmd);
-			}
-		}
-		catch (final Exception e)
-		{
-			LOGGER.error("There was an error while registering the commands!", e);
-		}
-	}
+    static
+    {
+        try (final var result = new ClassGraph().acceptPackages("dev.mlnr.spidey.commands").scan())
+        {
+            for (final var cls : result.getAllClasses())
+            {
+                final var cmd = (Command) cls.loadClass().getDeclaredConstructor().newInstance();
+                COMMANDS.put(cmd.getInvoke(), cmd);
+                for (final var alias : cmd.getAliases())
+                    COMMANDS.put(alias, cmd);
+            }
+        }
+        catch (final Exception e)
+        {
+            LOGGER.error("There was an error while registering the commands!", e);
+        }
+    }
 
-	public static void handle(final GuildMessageReceivedEvent event, final String prefix)
-	{
-		final var message = event.getMessage();
-		final var content = message.getContentRaw().substring(prefix.length()).trim();
-		if (content.isEmpty())
-		{
-			Utils.returnError("Please specify a command", message);
-			return;
-		}
-		final var command = (content.contains(" ") ? content.substring(0, content.indexOf(' ')) : content).toLowerCase();
-		final var cmd = COMMANDS.get(command);
-		if (cmd == null)
-		{
-			final var similar = StringUtils.getSimilarCommand(command);
-			Utils.returnError("**" + command + "** isn't a valid command. " + (similar == null ? "Check `" + prefix + "help` for a list of commands." : "Did you perhaps mean **" + similar + "**?"), message, false);
-			return;
-		}
-		final var requiredPermission = cmd.getRequiredPermission();
-		final var member = message.getMember();
-		final var userId = member.getIdLong();
-		if (!member.hasPermission(requiredPermission))
-		{
-			Utils.returnError(String.format(NO_PERMS, requiredPermission.getName()), message);
-			return;
-		}
-		final var guildId = message.getGuild().getIdLong();
-		if (isOnCooldown(userId, cmd))
-		{
-			Utils.returnError("The command is on cooldown", message);
-			return;
-		}
+    public static void handle(final GuildMessageReceivedEvent event, final String prefix)
+    {
+        final var message = event.getMessage();
+        final var content = message.getContentRaw().substring(prefix.length()).trim();
+        if (content.isEmpty())
+        {
+            Utils.returnError("Please specify a command", message);
+            return;
+        }
+        final var command = (content.contains(" ") ? content.substring(0, content.indexOf(' ')) : content).toLowerCase();
+        final var cmd = COMMANDS.get(command);
+        if (cmd == null)
+        {
+            final var similar = StringUtils.getSimilarCommand(command);
+            Utils.returnError("**" + command + "** isn't a valid command. " + (similar == null ? "Check `" + prefix + "help` for a list of commands." : "Did you perhaps mean **" + similar + "**?"), message, false);
+            return;
+        }
+        final var requiredPermission = cmd.getRequiredPermission();
+        final var member = message.getMember();
+        final var userId = member.getIdLong();
+        if (!member.hasPermission(requiredPermission))
+        {
+            Utils.returnError(String.format(NO_PERMS, requiredPermission.getName()), message);
+            return;
+        }
+        final var guildId = message.getGuild().getIdLong();
+        if (isOnCooldown(userId, cmd))
+        {
+            Utils.returnError("The command is on cooldown", message);
+            return;
+        }
 
-		// NSFW COMMANDS HANDLING
-		final var channel = message.getTextChannel();
-		final var category = cmd.getCategory();
-		if (category == Category.NSFW)
-		{
-			if (!channel.isNSFW())
-			{
-				Utils.returnError("You can use nsfw commands only in nsfw channels", message);
-				return;
-			}
-			Utils.sendMessage(channel, KSoftAPIHelper.getNsfwImage(cmd.getInvoke(), member));
-			cooldown(guildId, userId, cmd);
-			return;
-		}
-		//
+        // NSFW COMMANDS HANDLING
+        final var channel = message.getTextChannel();
+        final var category = cmd.getCategory();
+        if (category == Category.NSFW)
+        {
+            if (!channel.isNSFW())
+            {
+                Utils.returnError("You can use nsfw commands only in nsfw channels", message);
+                return;
+            }
+            Utils.sendMessage(channel, KSoftAPIHelper.getNsfwImage(cmd.getInvoke(), member));
+            cooldown(guildId, userId, cmd);
+            return;
+        }
+        //
 
-		final var maxArgs = cmd.getMaxArgs();
-		final var tmp = content.split("\\s+", maxArgs > 0 ? maxArgs + 1 : 0);
-		final var args = Arrays.copyOfRange(tmp, 1, tmp.length);
-		cmd.execute(args, new CommandContext(event));
-		cooldown(guildId, userId, cmd);
-	}
-	
-	public static Map<String, Command> getCommands()
-	{
-		return COMMANDS;
-	}
+        final var maxArgs = cmd.getMaxArgs();
+        final var tmp = content.split("\\s+", maxArgs > 0 ? maxArgs + 1 : 0);
+        final var args = Arrays.copyOfRange(tmp, 1, tmp.length);
+        cmd.execute(args, new CommandContext(event));
+        cooldown(guildId, userId, cmd);
+    }
+
+    public static Map<String, Command> getCommands()
+    {
+        return COMMANDS;
+    }
 }
