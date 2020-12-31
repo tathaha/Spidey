c960b06b8abedfafd6ea85c0e2a47b0863876138
caneleex <git@mlnr.dev>
2020-12-31 19:11:52 +0100

    add botlist.space to stats apis

--- src/main/java/dev/mlnr/spidey/utils/requests/api/API.java
+++ src/main/java/dev/mlnr/spidey/utils/requests/api/API.java
@@ -3,7 +3,6 @@ package dev.mlnr.spidey.utils.requests.api;
 public enum API
 {
     KSOFT_NSFW("https://api.ksoft.si/images/rand-reddit/%s?span=month", "Bearer " + System.getenv("ksoft")),
-    TOP_GG("https://top.gg/api/bots/772446532560486410", System.getenv("topgg")),
     URBAN_DICTIONARY("http://api.urbandictionary.com/v0/define?term=%s", null);
 
     private final String url;
@@ -27,7 +26,8 @@ public enum API
 
     public enum Stats
     {
-        TOP_GG("https://top.gg/api/bots/%s/stats", "server_count", System.getenv("topgg"));
+        TOP_GG("https://top.gg/api/bots/%s/stats", "server_count", System.getenv("topgg")),
+        BOTLIST_SPACE("https://api.botlist.space/v1/bots/%s", "server_count", System.getenv("botlistspace"));
 
         private final String url;
         private final String statsParam;
