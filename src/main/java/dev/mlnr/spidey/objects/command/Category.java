a5be8b522328e29c760bb2ea734ff05d2602fd66
caneleex <git@mlnr.dev>
2021-01-22 14:22:15 +0100

    music category: remove 'in development' suffix

--- src/main/java/dev/mlnr/spidey/objects/command/Category.java
+++ src/main/java/dev/mlnr/spidey/objects/command/Category.java
@@ -7,7 +7,7 @@ public enum Category
     INFORMATIVE("\u2139 Informative"),
     NSFW("\uD83D\uDD1E NSFW"),
     FUN("\uD83D\uDE03 Fun"),
-    MUSIC("\uD83C\uDFB6 Music (in development)"),
+    MUSIC("\uD83C\uDFB6 Music"),
     SETTINGS("\u2699\uFE0F Settings");
 
     private final String friendlyName;
