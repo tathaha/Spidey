4bddc0cf7a24dba3127559be1a4604222998022d
caneleex <git@mlnr.dev>
2021-03-28 00:29:26 +0100

    separate mentionables parsing again

--- src/main/java/dev/mlnr/spidey/utils/ArgumentUtils.java
+++ src/main/java/dev/mlnr/spidey/utils/ArgumentUtils.java
@@ -34,98 +34,139 @@ public class ArgumentUtils {
 		}
 	}
 
+	// TODO somehow unify mentionable parsing (except user)
 	public static void parseArgumentAsTextChannel(String argument, CommandContext ctx, Consumer<TextChannel> consumer) {
-		parseArgumentAsMentionable(argument, ctx, mentionable -> consumer.accept(((TextChannel) mentionable)), Message.MentionType.CHANNEL);
+		var i18n = ctx.getI18n();
+		var notFound = i18n.get("argument_parser.not_found.channel") + " " + i18n.get("argument_parser.not_found.text");
+		var guild = ctx.getGuild();
+		var embedBuilder = Utils.createEmbedBuilder(ctx.getAuthor());
+
+		if (Message.MentionType.CHANNEL.getPattern().matcher(argument).matches()) {
+			var mentionedChannels = ctx.getMessage().getMentionedChannels();
+			if (mentionedChannels.isEmpty()) {
+				ctx.replyError(notFound);
+				return;
+			}
+			consumer.accept(mentionedChannels.get(0));
+		}
+		else if (ID_REGEX.matcher(argument).matches()) {
+			var channelId = Long.parseLong(argument);
+			var channel = guild.getTextChannelById(channelId);
+			if (channel == null) {
+				ctx.replyError(notFound);
+				return;
+			}
+			consumer.accept(channel);
+		}
+		else if (argument.length() >= 2 && argument.length() <= 32) {
+			var textChannels = guild.getTextChannelsByName(argument, true);
+			if (textChannels.isEmpty()) {
+				ctx.replyErrorLocalized("argument_parser.not_found.name", i18n.get("argument_parser.not_found.channel").toLowerCase());
+				return;
+			}
+			if (textChannels.size() == 1) {
+				consumer.accept(textChannels.get(0));
+				return;
+			}
+			createMentionableSelection(embedBuilder, textChannels, ctx, "channel", channel -> channel.getAsMention() + " - ID: " + channel.getIdLong(),
+					choice -> consumer.accept(textChannels.get(choice)));
+		}
+		else {
+			ctx.reply(notFound);
+		}
 	}
 
 	public static void parseArgumentAsRole(String argument, CommandContext ctx, Consumer<Role> consumer) {
-		parseArgumentAsMentionable(argument, ctx, mentionable -> consumer.accept(((Role) mentionable)), Message.MentionType.ROLE);
+		var i18n = ctx.getI18n();
+		var notFound = i18n.get("argument_parser.not_found.role") + " " + i18n.get("argument_parser.not_found.text");
+		var guild = ctx.getGuild();
+		var embedBuilder = Utils.createEmbedBuilder(ctx.getAuthor());
+
+		if (Message.MentionType.ROLE.getPattern().matcher(argument).matches()) {
+			var mentionedRoles = ctx.getMessage().getMentionedRoles();
+			if (mentionedRoles.isEmpty()) {
+				ctx.replyError(notFound);
+				return;
+			}
+			consumer.accept(mentionedRoles.get(0));
+		}
+		else if (ID_REGEX.matcher(argument).matches()) {
+			var roleId = Long.parseLong(argument);
+			var role = guild.getRoleById(roleId);
+			if (role == null) {
+				ctx.replyError(notFound);
+				return;
+			}
+			consumer.accept(role);
+		}
+		else if (argument.length() >= 2 && argument.length() <= 32) {
+			var roles = guild.getRolesByName(argument, true);
+			if (roles.isEmpty()) {
+				ctx.replyErrorLocalized("argument_parser.not_found.name", i18n.get("argument_parser.not_found.role").toLowerCase());
+				return;
+			}
+			if (roles.size() == 1) {
+				consumer.accept(roles.get(0));
+				return;
+			}
+			createMentionableSelection(embedBuilder, roles, ctx, "role", role -> role.getAsMention() + " - ID: " + role.getIdLong(),
+					choice -> consumer.accept(roles.get(choice)));
+		}
+		else {
+			ctx.reply(notFound);
+		}
 	}
 
 	public static void parseArgumentAsUser(String argument, CommandContext ctx, Consumer<User> consumer) {
-		parseArgumentAsMentionable(argument, ctx, mentionable -> consumer.accept(((User) mentionable)), Message.MentionType.USER);
-	}
-
-	private static void parseArgumentAsMentionable(String argument, CommandContext ctx, Consumer<IMentionable> consumer, Message.MentionType type) {
-		var message = ctx.getMessage();
-		var guild = ctx.getGuild();
-		var author = ctx.getAuthor();
-		var typeName = type.name().toLowerCase();
 		var i18n = ctx.getI18n();
-		var notFound = i18n.get("argument_parser.not_found." + typeName) + " " + i18n.get("argument_parser.not_found.text");
+		var message = ctx.getMessage();
+		var notFound = i18n.get("argument_parser.not_found.user") + " " + i18n.get("argument_parser.not_found.text");
+		var author = ctx.getAuthor();
 		var embedBuilder = Utils.createEmbedBuilder(author);
 
-		if (type.getPattern().matcher(argument).matches()) {
-			var mentions = message.getMentions(type);
-			if (mentions.isEmpty()) {
+		if (Message.MentionType.USER.getPattern().matcher(argument).matches()) {
+			var mentionedUsers = message.getMentionedUsers();
+			if (mentionedUsers.isEmpty()) {
 				ctx.replyError(notFound);
 				return;
 			}
-			consumer.accept(mentions.get(0));
-			return;
+			consumer.accept(mentionedUsers.get(0));
 		}
-
-		var idMatcher = ID_REGEX.matcher(argument);
-		if (idMatcher.matches()) {
-			var mentionableId = Long.parseLong(idMatcher.group());
-			if (type == Message.MentionType.USER) {
-				if (mentionableId == author.getIdLong()) {
-					consumer.accept(author);
+		else if (ID_REGEX.matcher(argument).matches()) {
+			var userId = Long.parseLong(argument);
+			if (userId == author.getIdLong()) {
+				consumer.accept(author);
+				return;
+			}
+			var jda = ctx.getJDA();
+			var selfUser = jda.getSelfUser();
+			if (userId == selfUser.getIdLong()) {
+				consumer.accept(selfUser);
+				return;
+			}
+			jda.retrieveUserById(userId).queue(consumer, failure -> ctx.replyError(notFound));
+		}
+		else if (argument.length() >= 2 && argument.length() <= 32) {
+			if (argument.equalsIgnoreCase(ctx.getMember().getEffectiveName())) {
+				consumer.accept(author);
+				return;
+			}
+			message.getGuild().retrieveMembersByPrefix(argument, 10).onSuccess(members -> {
+				if (members.isEmpty()) {
+					ctx.replyError(notFound);
 					return;
 				}
-				var jda = ctx.getJDA();
-				var selfUser = jda.getSelfUser();
-				if (mentionableId == selfUser.getIdLong()) {
-					consumer.accept(selfUser);
+				if (members.size() == 1) {
+					consumer.accept(members.get(0).getUser());
 					return;
 				}
-				jda.retrieveUserById(mentionableId).queue(consumer, failure -> ctx.replyError(notFound));
-				return;
-			}
-			var mentionable = type == Message.MentionType.CHANNEL ? guild.getTextChannelById(mentionableId) : guild.getRoleById(mentionableId);
-			if (mentionable == null) {
-				ctx.replyError(notFound);
-				return;
-			}
-			consumer.accept(mentionable);
-			return;
+				createMentionableSelection(embedBuilder, members.stream().map(Member::getUser).collect(Collectors.toList()), ctx, "user",
+						mentionable -> mentionable.getAsMention() + " - **" + ((User) mentionable).getAsTag() + "**", choice -> consumer.accept(members.get(choice).getUser()));
+			});
 		}
-
-		if (argument.length() >= 2 && argument.length() <= 32) {
-			if (type == Message.MentionType.USER) {
-				if (argument.equalsIgnoreCase(ctx.getMember().getEffectiveName())) {
-					consumer.accept(author);
-					return;
-				}
-				message.getGuild().retrieveMembersByPrefix(argument, 10)
-						.onSuccess(members -> {
-							if (members.isEmpty()) {
-								ctx.replyError(notFound);
-								return;
-							}
-							if (members.size() == 1) {
-								consumer.accept(members.get(0).getUser());
-								return;
-							}
-							createMentionableSelection(embedBuilder, members.stream().map(Member::getUser).collect(Collectors.toList()), ctx, "user",
-									mentionable -> mentionable.getAsMention() + " - **" + ((User) mentionable).getAsTag() + "**", choice -> consumer.accept(members.get(choice).getUser()));
-						});
-				return;
-			}
-			var mentionables = type == Message.MentionType.CHANNEL ? guild.getTextChannelsByName(argument, true) : guild.getRolesByName(argument, true);
-			if (mentionables.isEmpty()) {
-				ctx.replyErrorLocalized("argument_parser.not_found.name", i18n.get("argument_parser.not_found." + typeName).toLowerCase());
-				return;
-			}
-			if (mentionables.size() == 1) {
-				consumer.accept(mentionables.get(0));
-				return;
-			}
-			createMentionableSelection(embedBuilder, mentionables, ctx, typeName, mentionable -> mentionable.getAsMention() + " - ID: " + mentionable.getIdLong(),
-					choice -> consumer.accept(mentionables.get(choice)));
-			return;
+		else {
+			ctx.reply(notFound);
 		}
-		ctx.replyError(notFound);
 	}
 
 	private static void createMentionableSelection(EmbedBuilder selectionBuilder, List<? extends IMentionable> mentionables, CommandContext ctx, String type,
