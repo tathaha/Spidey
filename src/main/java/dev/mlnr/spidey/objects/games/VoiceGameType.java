722fa0aeae5197ac31d0c08119ed9709f6419cc2
caneleex <git@mlnr.dev>
2021-05-07 13:04:09 +0200

    uncomment poker night

--- src/main/java/dev/mlnr/spidey/objects/games/VoiceGameType.java
+++ src/main/java/dev/mlnr/spidey/objects/games/VoiceGameType.java
@@ -2,7 +2,7 @@ package dev.mlnr.spidey.objects.games;
 
 public enum VoiceGameType {
 	YOUTUBE_TOGETHER(755600276941176913L, "YouTube Together", new String[]{"youtubetogether", "yt2gether", "yttogether", "youtube2gether"}),
-	//POKER_NIGHT(755827207812677713L, "Poker Night", new String[]{"poker", "pokernight"}), - currently broken
+	POKER_NIGHT(755827207812677713L, "Poker Night", new String[]{"poker", "pokernight"}),
 	BETRAYAL_IO(773336526917861400L, "Betrayal.io", new String[]{"betrayalio", "betrayal"}),
 	FISHINGTON_IO(814288819477020702L, "Fishington.io", new String[]{"fishingtionio", "fishington"});
 
