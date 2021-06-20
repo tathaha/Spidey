dd585a4ff3b03f2cb2d23a740dfb0ffb1635f713
caneleex <git@mlnr.dev>
2021-06-20 16:26:39 +0200

    uncomment chess game

--- src/main/java/dev/mlnr/spidey/objects/games/VoiceGameType.java
+++ src/main/java/dev/mlnr/spidey/objects/games/VoiceGameType.java
@@ -6,8 +6,8 @@ public enum VoiceGameType implements ChoicesEnum {
 	YOUTUBE_TOGETHER("YouTube Together", 755600276941176913L),
 	POKER_NIGHT("Poker Night", 755827207812677713L),
 	BETRAYAL_IO("Betrayal.io", 773336526917861400L),
-	FISHINGTON_IO("Fishington.io", 814288819477020702L);
-	// CHESS("Chess Game", 832012586023256104L);
+	FISHINGTON_IO("Fishington.io", 814288819477020702L),
+	CHESS("Chess Game", 832012586023256104L);
 
 	private final String friendlyName;
 	private final long applicationId;
