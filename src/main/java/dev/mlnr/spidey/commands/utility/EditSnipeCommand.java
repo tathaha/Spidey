6d26109608dd2d9b638280518e69858514638215
caneleex <git@mlnr.dev>
2021-08-26 21:29:18 +0200

    change author jump to a button

--- src/main/java/dev/mlnr/spidey/commands/utility/EditSnipeCommand.java
+++ src/main/java/dev/mlnr/spidey/commands/utility/EditSnipeCommand.java
@@ -5,6 +5,7 @@ import dev.mlnr.spidey.objects.command.CommandContext;
 import dev.mlnr.spidey.objects.command.category.Category;
 import dev.mlnr.spidey.utils.Utils;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.interactions.components.Button;
 
 import java.awt.*;
 
@@ -35,8 +36,10 @@ public class EditSnipeCommand extends Command {
 		embedBuilder.setColor(Color.GREEN);
 
 		ctx.getJDA().retrieveUserById(lastEditedMessage.getAuthorId()).queue(user -> {
-			embedBuilder.setAuthor(user.getName(), lastEditedMessage.getJumpUrl(), user.getEffectiveAvatarUrl());
-			ctx.reply(embedBuilder);
+			embedBuilder.setAuthor(user.getName(), null, user.getEffectiveAvatarUrl());
+
+			var jumpButton = Button.link(lastEditedMessage.getJumpUrl(), ctx.getI18n().get("commands.editsnipe.jump"));
+			ctx.replyWithComponents(embedBuilder, jumpButton);
 		});
 		return true;
 	}
