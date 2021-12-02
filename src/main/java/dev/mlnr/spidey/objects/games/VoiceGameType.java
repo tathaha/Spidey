33b5652305cbe4b2250a321cff272c1cacc971ca
caneleex <git@mlnr.dev>
2021-12-02 20:11:35 +0100

    add new activities and remove betrayal and fishington.io

--- src/main/java/dev/mlnr/spidey/objects/games/VoiceGameType.java
+++ src/main/java/dev/mlnr/spidey/objects/games/VoiceGameType.java
@@ -3,11 +3,14 @@ package dev.mlnr.spidey.objects.games;
 import dev.mlnr.spidey.objects.command.ChoicesEnum;
 
 public enum VoiceGameType implements ChoicesEnum {
-	YOUTUBE_TOGETHER("YouTube Together", 880218394199220334L),
 	POKER_NIGHT("Poker Night", 755827207812677713L),
-	BETRAYAL_IO("Betrayal.io", 773336526917861400L),
-	FISHINGTON_IO("Fishington.io", 814288819477020702L),
-	CHESS_IN_THE_PARK("Chess In The Park", 832012774040141894L);
+	CHESS_IN_THE_PARK("Chess In The Park", 832012774040141894L),
+	DOODLE_CREW("Doodle Crew", 878067389634314250L),
+	LETTER_TILE("Letter Tile", 879863686565621790L),
+	SPELLCAST("SpellCast", 852509694341283871L),
+	WATCH_TOGETHER("Watch Together", 880218394199220334L),
+	CHECKERS_IN_THE_PARK("Checkers In The Park", 832013003968348200L),
+	WORD_SNACKS("Word Snacks", 879863976006127627L);
 
 	private final String friendlyName;
 	private final long applicationId;
