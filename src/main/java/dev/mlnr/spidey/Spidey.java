bb45bed534b19b19ecb8ef854a6b32d5426b63ca
caneleex <git@mlnr.dev>
2022-03-18 16:52:30 +0100

    modify ReadyEvents to be compliant with sharding

--- src/main/java/dev/mlnr/spidey/Spidey.java
+++ src/main/java/dev/mlnr/spidey/Spidey.java
@@ -1,9 +1,13 @@
 package dev.mlnr.spidey;
 
 import com.sedmelluq.discord.lavaplayer.jdaudp.NativeAudioSendFactory;
+import dev.mlnr.blh.core.api.BLHBuilder;
+import dev.mlnr.blh.core.api.BotList;
+import dev.mlnr.blh.jda.BLHJDAUpdater;
 import dev.mlnr.spidey.events.ReadyEvents;
 import dev.mlnr.spidey.objects.I18n;
 import dev.mlnr.spidey.utils.MusicUtils;
+import dev.mlnr.spidey.utils.Utils;
 import net.dv8tion.jda.api.GatewayEncoding;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
@@ -19,12 +23,16 @@ import org.slf4j.LoggerFactory;
 
 import javax.security.auth.login.LoginException;
 import java.util.EnumSet;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import static net.dv8tion.jda.api.requests.GatewayIntent.*;
 import static net.dv8tion.jda.api.utils.cache.CacheFlag.*;
 
 public class Spidey {
 	private static final Logger logger = LoggerFactory.getLogger(Spidey.class);
+	private static final String BOTLIST_ENV_PREFIX = "BOTLIST_";
 	private final ShardManager shardManager;
 
 	private final DatabaseManager databaseManager = new DatabaseManager();
@@ -56,6 +64,18 @@ public class Spidey {
 			.setStatus(OnlineStatus.DO_NOT_DISTURB)
 			.setGatewayEncoding(GatewayEncoding.ETF)
 			.setAudioSendFactory(new NativeAudioSendFactory())
+			.build();
+
+		var botLists = System.getenv().entrySet()
+			.stream()
+			.filter(entry -> entry.getKey().startsWith(BOTLIST_ENV_PREFIX))
+			.collect(Collectors.toMap(entry -> BotList.valueOf(entry.getKey().substring(BOTLIST_ENV_PREFIX.length())), Map.Entry::getValue));
+		new BLHBuilder(new BLHJDAUpdater(shardManager), botLists).setDevModePredicate(botId -> botId != Utils.SPIDEY_ID)
+			.setSuccessLoggingEnabled(false)
+			.setUnavailableEventsEnabled(false)
+			.setErrorLoggingThreshold(2)
+			.setNoUpdateNecessaryLoggingEnabled(false)
+			.setAutoPostDelay(5, TimeUnit.MINUTES)
 			.build();
 	}
 
